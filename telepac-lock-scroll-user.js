03f4535692a4c48b877b2abb3e1e6b713a96f2ce
zakk44430 <zakk44430@gmail.com>
2026-03-11 19:55:09 +0100

    Create telepac-lock-scroll-user.js

---
+++ telepac-lock-scroll-user.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         Telepac Lock Scroll Top
+// @namespace    https://github.com/zakk44
+// @version      1.0
+// @description  Prevent Telepac interface from scrolling down
+// @author       zakk44
+// @match        https://isis.telepac.agriculture.gouv.fr/telepac/prg/rpg/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    function lockScroll() {
+        if (window.scrollY > 0) {
+            window.scrollTo(0, 0);
+        }
+    }
+
+    // Bloque scroll souris
+    window.addEventListener('scroll', lockScroll, { passive: true });
+
+    // Bloque certaines touches clavier
+    window.addEventListener('keydown', function(e) {
+        const blockedKeys = [
+            "ArrowDown",
+            "PageDown",
+            "End",
+            " "
+        ];
+
+        if (blockedKeys.includes(e.key)) {
+            e.preventDefault();
+        }
+    });
+
+    // Au chargement
+    window.addEventListener('load', () => {
+        window.scrollTo(0, 0);
+    });
+
+})();
